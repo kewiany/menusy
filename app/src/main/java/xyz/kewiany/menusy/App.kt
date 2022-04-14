bb2450d895fdff0f87c168019126b701b4cc58c2
kewiany <kewin.czuprynski@fara.no>
2022-04-15 01:13:34 +0200

    Add bottom bar, add scaffold

--- app/src/main/java/xyz/kewiany/menusy/App.kt
+++ app/src/main/java/xyz/kewiany/menusy/App.kt
@@ -1,16 +1,26 @@
 package xyz.kewiany.menusy
 
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import xyz.kewiany.menusy.ui.BottomBar
 import xyz.kewiany.menusy.ui.menu.MenuViewModel
 import xyz.kewiany.menusy.ui.utils.NavGraph
 import xyz.kewiany.menusy.ui.welcome.WelcomeViewModel
 
 @Composable
 fun App() {
+    val navController: NavHostController = rememberNavController()
     val welcomeViewModel = WelcomeViewModel()
     val menuViewModel = MenuViewModel()
-    NavGraph(
-        welcomeViewModel = welcomeViewModel,
-        menuViewModel = menuViewModel
-    )
+    Scaffold(
+        bottomBar = { BottomBar(navController) }
+    ) {
+        NavGraph(
+            navController = navController,
+            welcomeViewModel = welcomeViewModel,
+            menuViewModel = menuViewModel
+        )
+    }
 }
---
+++ app/src/main/java/xyz/kewiany/menusy/ui/BottomBar.kt
@@ -0,0 +1,44 @@
+package xyz.kewiany.menusy.ui
+
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import xyz.kewiany.menusy.ui.utils.Navigation.Destination.WELCOME_PATH
+
+
+@Composable
+fun BottomBar(
+    navController: NavHostController = rememberNavController()
+) {
+    val selectedIndex = remember { mutableStateOf(0) }
+    BottomNavigation(elevation = 10.dp) {
+        BottomNavigationItem(icon = {
+            Icon(imageVector = Icons.Default.Home, "")
+        },
+            label = { Text(text = "Menu") },
+            selected = (selectedIndex.value == 0),
+            onClick = {
+                navController.navigate(WELCOME_PATH)
+                selectedIndex.value = 0
+            })
+
+        BottomNavigationItem(icon = {
+            Icon(imageVector = Icons.Default.Add, "")
+        },
+            label = { Text(text = "Order") },
+            selected = (selectedIndex.value == 1),
+            onClick = {
+                selectedIndex.value = 1
+            })
+    }
+}
