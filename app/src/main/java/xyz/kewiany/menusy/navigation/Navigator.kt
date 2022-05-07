297dd966196e1e9dc574cdbf713e4e93d512893e
kewiany <kewin.czuprynski@fara.no>
2022-05-07 16:48:03 +0200

    Make singleton

--- app/src/main/java/xyz/kewiany/menusy/navigation/Navigator.kt
+++ app/src/main/java/xyz/kewiany/menusy/navigation/Navigator.kt
@@ -3,7 +3,9 @@ package xyz.kewiany.menusy.navigation
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class Navigator @Inject constructor() {
 
     private val _commands = MutableSharedFlow<NavigationCommand>(extraBufferCapacity = 1)
