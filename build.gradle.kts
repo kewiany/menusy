7591dcc013302e24d3bbf00e945e829ab0a4f4ca
kewiany <kewin.czuprynski@fara.no>
2022-05-31 00:15:44 +0200

    Update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id(BuildPlugin.androidApplication) version "7.3.0-alpha01" apply false
+    id(BuildPlugin.androidApplication) version "7.4.0-alpha02" apply false
     id(BuildPlugin.androidLibrary) version "7.3.0-alpha01" apply false
     id(BuildPlugin.kotlinAndroid) version Versions.kotlin apply false
 }
