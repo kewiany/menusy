7e78acfdd6e346401546d3d83fee2dacb5eb4f4b
kewiany <kewin.czuprynski@fara.no>
2022-09-12 21:36:14 +0200

    Add check circle to represent ordered product

--- app/src/main/java/xyz/kewiany/menusy/presentation/features/menu/items/ui/ProductItem.kt
+++ app/src/main/java/xyz/kewiany/menusy/presentation/features/menu/items/ui/ProductItem.kt
@@ -7,8 +7,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
@@ -52,8 +56,22 @@ fun ProductItem(
             }
             Row {
                 Text(
-                    text = description
+                    text = description,
+                    modifier = Modifier.weight(0.8f)
                 )
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier
+                        .weight(0.2f)
+                ) {
+                    if (ordered) {
+                        Icon(
+                            imageVector = Icons.Default.CheckCircle,
+                            contentDescription = "Ordered",
+                            tint = Color.White
+                        )
+                    }
+                }
             }
             if (ordered) {
                 Row {
