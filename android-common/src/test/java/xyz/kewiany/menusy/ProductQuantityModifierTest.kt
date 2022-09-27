a7875c251323057d2c3950c77ee0dc73c1bd831d
kewiany <kewin.czuprynski@fara.no>
2022-09-28 00:12:36 +0200

    Test product quantity

--- android-common/src/test/java/xyz/kewiany/menusy/ProductQuantityModifierTest.kt
+++ android-common/src/test/java/xyz/kewiany/menusy/ProductQuantityModifierTest.kt
@@ -1,7 +1,6 @@
 package xyz.kewiany.menusy
 
-import org.junit.Assert.assertEquals
-import org.junit.Assert.assertTrue
+import org.junit.Assert.*
 import org.junit.Test
 import xyz.kewiany.menusy.android.common.ProductQuantityModifier
 import xyz.kewiany.menusy.android.common.ProductUiItem
@@ -71,5 +70,63 @@ class ProductQuantityModifierTest {
 
         assertEquals(2, newQuantity)
         assertTrue((newItems.first { it.id == product.id } as ProductUiItem).quantity == 2)
+    }
+
+    @Test
+    fun decreaseQuantityOnOneProductQuantity() {
+        val product = createProductUiItem(
+            quantity = 0,
+            product = createProduct(
+                id = "id2"
+            )
+        )
+        val products = listOf(
+            createProductUiItem(
+                quantity = 0,
+                product = createProduct(
+                    id = "id1",
+                ),
+            ),
+            product,
+            createProductUiItem(
+                quantity = 0,
+                product = createProduct(
+                    id = "id3"
+                )
+            ),
+        )
+        val pair = productQuantityModifier.decreaseQuantity(products, product.id)
+        assertNull(pair)
+    }
+
+    @Test
+    fun decreaseQuantityOnTwoProductQuantity() {
+        val product = createProductUiItem(
+            quantity = 2,
+            product = createProduct(
+                id = "id2"
+            )
+        )
+        val products = listOf(
+            createProductUiItem(
+                quantity = 0,
+                product = createProduct(
+                    id = "id1",
+                ),
+            ),
+            product,
+            createProductUiItem(
+                quantity = 0,
+                product = createProduct(
+                    id = "id3"
+                )
+            ),
+        )
+        val (newQuantity, newItems) = requireNotNull(
+            productQuantityModifier.decreaseQuantity(products, product.id)
+        )
+
+        assertEquals(1, newQuantity)
+        assertTrue((newItems.first { it.id == product.id } as ProductUiItem).quantity == 1)
     }
 }
